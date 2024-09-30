aa819191f58375f9a94cdd54a6a9c4960914e546
HD-MOE <tok_hong_da@moe.edu.sg>
2024-09-30 18:38:48 +0800

    Added Features

--- Example2.py
+++ Example2.py
@@ -7,16 +7,17 @@ import streamlit as st
 #create a value in st.session_state and 
 #use the button to set that value to True in a callback.
 
-if 'clicked' not in st.session_state: 
+if 'clicked!!' not in st.session_state: 
 	st.session_state.clicked = False 
 	
 def click_button(): 
 	st.session_state.clicked = True 
 	
 st.button('Click me', on_click = click_button)  #on_click is always true
-st.session_state.clicked
 
 if st.session_state.clicked: 
 	# The message and nested widget will remain on the page 
 	st.write('Button clicked!') 
 	st.slider('Select a value')
+
+print("Warning")
